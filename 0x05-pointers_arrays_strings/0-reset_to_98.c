b09700d8e4481d45b65924e190a6f5e2f4596a72
Kingsley <elfinalist@gmail.com>
2022-03-22 14:09:55 +0300

    function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,8 @@
-#include <main.h>
+#include "main.h"
 
 int main()
 
+void reset_to_98(int *n)
 {
-
-    int shopping = 20;
-    int *p = &shopping;
-
-    printf("Shopping: %d\n", shopping) 
-    printf("Shopping(through pointer): %d\n", *p);
-
-    *p = 98
-
-    printf("Shopping: %d\n", shopping) 
-    printf("Shopping(through pointer): %d\n", *p);
-
-    return 0;
+    *n = 98;
 }
