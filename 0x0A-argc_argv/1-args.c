18b58da9605620094c04b76e4b46d216086fe224
Kingsley <elfinalist@gmail.com>
2022-04-04 09:41:13 +0300

    program that prints the number of arguments passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,17 +2,15 @@
 #include <stdlib.h>
 
 /**
-*main - Entry point
-*
-*@argc: counts the number of parameters that go into main
-*@argv: Pointer or array of pointers containing strings entering main
-*Return: Always 0 (Success)
-*/
-
-int main (int argc, char **argv)
+ * main - Entry point
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
 {
-
-    (void) argv;
-    printf("%i\n", argc - 1);
-    return (0);
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
 }
