4094f2259f9f3d12cc56d5a4e695f10318596841
Kingsley <elfinalist@gmail.com>
2022-05-04 00:24:36 +0300

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,49 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: Name of the file or path
- * @letters: Number of letters it should read and print
- *
- * Return: Actual number of letters it could read and print
- **/
+ * read_textfile - a function that reads a text file and prints
+ * to the POSIX STDOUT
+ * @filename: the filename to open
+ * @letters: the number of letters to read and print
+ * Return: the number of letters read and prints, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, n_bytes, status;
-	char *buffer;
+	int fdo, fdr, fdw;
+	char *temp;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	temp = malloc(sizeof(char) * letters);
+	if (temp == NULL)
 		return (0);
 
-	buffer = malloc(letters * sizeof(char));
-	if (buffer == NULL)
-		return (0);
-
-	n_bytes = read(fd, buffer, letters);
-	buffer[n_bytes] = 0;
-	close(fd);
-
-	if (n_bytes == 0)
+	fdo = open(filename, O_RDONLY);
+	if (fdo < 0)
 	{
-		free(buffer);
+		free(temp);
 		return (0);
 	}
 
-	status = write(STDOUT_FILENO, buffer, n_bytes);
-	free(buffer);
+	fdr = read(fdo, temp, letters);
+	if (fdr < 0)
+	{
+		free(temp);
+		return (0);
+	}
 
-	return (status);
+	fdw = write(STDOUT_FILENO, temp, fdr);
+	free(temp);
+	close(fdo);
+
+	if (fdw < 0)
+		return (0);
+	return ((ssize_t)fdw);
 }
